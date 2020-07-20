20a97617bf97415a194c61a5a2af82641bd4d8d9
Leif Madsen <lmadsen@redhat.com>
2020-07-20 14:18:29 -0400

    Add Travis-CI and annocheck validation

---
+++ build/build_checks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+dnf install make gcc qpid-proton-c-devel annobin-annocheck -y
+
+make 'CFLAGS=-O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fexceptions -fstack-protector-strong -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection' 'LDFLAGS=-Wl,-z,relro  -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld'
+
+annocheck bridge
