b956d7e002d595d5edc1d6564536243eee703c26
Leif Madsen <lmadsen@redhat.com>
2020-07-20 14:37:22 -0400

    Add missing dependency

--- build/build_checks.sh
+++ build/build_checks.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-dnf install make gcc qpid-proton-c-devel annobin-annocheck -y
+dnf install make gcc qpid-proton-c-devel annobin-annocheck rpm-build -y
 
 make 'CFLAGS=-O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fexceptions -fstack-protector-strong -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection' 'LDFLAGS=-Wl,-z,relro  -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld'
 
